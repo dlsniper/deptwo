ddb6110aa83d4ce924e10cce23f54134d3d5d123
Florin Patan <florinpatan@gmail.com>
2018-05-19 11:59:35 +0300

    Initial commit

---
+++ dep2.go
@@ -0,0 +1,14 @@
+package depone
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
+type MyDepTwo string
+
+func (m MyDepTwo) Hello() {
+	fmt.Printf("Hello from MyDepTwo, %s", m)
+	logrus.Println("called: MyDepTwo.Hello()")
+}
